5ac114daf08f3337cab1a82d225d1db884453ea8
Dmitry Anufriev <dm.anufriev@gmail.com>
2024-06-29 21:00:29 +0300

    Step 3. Create model, base repository, schema.sql. Add tests.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
@@ -11,17 +14,25 @@ import java.sql.SQLException;
 public class App {
 
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("DB_PORT", "7070");
         return Integer.valueOf(port);
     }
 
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
-
         app.start(getPort());
     }
 
     public static Javalin getApp() throws IOException, SQLException {
+
+        String jdbcDatabaseUrl = System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(jdbcDatabaseUrl);
+        var dataSource = new HikariDataSource(hikariConfig);
+        BaseRepository.dataSource = dataSource;
+
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
