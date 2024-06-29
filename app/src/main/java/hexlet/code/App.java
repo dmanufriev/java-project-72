3ccc2807196aa3b135fc72e27a6f63e1410e2075
Dmitry Anufriev <dm.anufriev@gmail.com>
2024-06-29 19:17:38 +0300

    Add start http page for application

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,40 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Slf4j
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        return app;
+    }
+}
