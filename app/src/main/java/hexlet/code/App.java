8a07e90275a29ae04ebf2cb7113bbfe41b7d67b4
Dmitry Anufriev <dm.anufriev@gmail.com>
2024-06-30 12:36:32 +0300

    Step 4. Add jte templates. Add h2 driver

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,31 +2,32 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.dto.MainPage;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
-import org.postgresql.Driver;
 import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static io.javalin.rendering.template.TemplateUtil.model;
+
 @Slf4j
 public class App {
 
-    private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.valueOf(port);
-    }
-
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         var app = getApp();
         app.start(getPort());
     }
 
     public static Javalin getApp() throws IOException, SQLException {
 
-        DriverManager.registerDriver(new Driver());
+        DriverManager.registerDriver(new org.postgresql.Driver());
+        DriverManager.registerDriver(new org.h2.Driver());
         DriverManager.drivers().forEach(d -> log.info(d.toString()));
 
         String jdbcDatabaseUrl = System.getenv()
@@ -45,7 +46,7 @@ public class App {
 
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
         app.before(ctx -> {
@@ -53,9 +54,26 @@ public class App {
         });
 
         app.get("/", ctx -> {
-            ctx.render("index.jte");
+            var page = new MainPage(false, null);
+            ctx.render("index.jte", model("page", page));
         });
 
         return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    // Чтобы при проверке приложения автотестами шаблоны подгружались из нужного места,
+    // нам потребуется явно указать расположение шаблонов.
+    // Для этого нужно создать инстанс движка шаблонизатора и,
+    // используя ResourceCodeResolver, указать в нем путь к шаблонам
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 }
