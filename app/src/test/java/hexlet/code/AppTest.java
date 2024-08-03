b9241535f10b9d926c839fe48fe9ad490c22c8db
Dmitry Anufriev <dm.anufriev@gmail.com>
2024-08-03 13:31:57 +0300

    Step 6. Add integration tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,12 +1,18 @@
 package hexlet.code;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlRepository;
+import hexlet.code.utils.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 public class AppTest {
     Javalin app;
@@ -19,9 +25,68 @@ public class AppTest {
     @Test
     public void testMainPage() {
         JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/");
+            var response = client.get(NamedRoutes.mainPath());
             assertThat(response.code()).isEqualTo(200);
             assertThat(response.body().string()).contains("Анализатор страниц");
+        });
+    }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlsPath());
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("Сайты");
+        });
+    }
+
+    @Test
+    public void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=https://test.com";
+            var response = client.post(NamedRoutes.urlsPath(), requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("https://test.com");
+        });
+    }
+
+    @Test
+    public void testCreateIncorrectUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=www.test.com";
+            var response = client.post(NamedRoutes.urlsPath(), requestBody);
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void testCreateSameUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var url = new Url("https://test.com", Timestamp.from(Instant.now()));
+            UrlRepository.save(url);
+            var requestBody = "url=https://test.com";
+            var response = client.post(NamedRoutes.urlsPath(), requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("https://test.com");
+        });
+    }
+
+    @Test
+    public void testUrlPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var url = new Url("https://test.com", Timestamp.from(Instant.now()));
+            UrlRepository.save(url);
+            var response = client.get(NamedRoutes.urlPath(url.getId()));
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains(url.getName());
+        });
+    }
+
+    @Test
+    public void testUrlPageNotFound() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlPath((long) 9999));
+            assertThat(response.code()).isEqualTo(404);
         });
     }
 }
